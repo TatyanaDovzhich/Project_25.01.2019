72a2626a76ab58497155bcb6342d06a1ed2e6cf2
Tatyana Dovzhich <dovzhich.tatyana@gmail.com>
2019-01-26 00:31:42 +0200

    Comment_25.01.2019

---
+++ file_25.01.2019.py
@@ -0,0 +1,27 @@
+def addition(a, b):
+      return a+b
+
+    
+print  (addition(25, 10))
+
+
+
+def subtraction (a, b):
+    return a-b
+
+
+print  (subtraction(25, 10))
+
+
+def multiplication (a, b):
+    return a*b
+
+
+print  (multiplication(25, 10))
+
+
+def division(a, b):
+    return a/b
+
+
+print  (division (25, 10))
